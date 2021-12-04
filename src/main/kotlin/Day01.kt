8d2bc4ee577169cbeb73e41fc4d9170cdf00a6c9
Thomas Wirth <thomas-w-@web.de>
2021-12-04 21:15:31 +0100

    Add day1 part2 puzzle

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -6,5 +6,16 @@ fun main() {
       .count { it.first() < it.last() }
   }
 
+  fun part2(input: List<String>): Int {
+    return input
+      .asSequence()
+      .mapNotNull { it.toIntOrNull() }
+      .windowed(3)
+      .map { it.sum() }
+      .windowed(2)
+      .count { it.first() < it.last() }
+  }
+
   println(part1(readInputAsLines("Day01.txt")))
+  println(part2(readInputAsLines("Day01.txt")))
 }
